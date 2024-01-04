a392f01f46ca85ca38c8d432efc0d5bd35eb96e0
concept <conceptcodes@gmail.com>
2024-01-03 20:45:02 -0500

    improvements for more context ;)

--- src/lib/ai.ts
+++ src/lib/ai.ts
@@ -1,115 +1,155 @@
 import { TextLoader } from "langchain/document_loaders/fs/text";
 import { ChatOpenAI } from "langchain/chat_models/openai";
 import { HNSWLib } from "@langchain/community/vectorstores/hnswlib";
-import { OpenAIEmbeddings } from "langchain/embeddings/openai";
-import { PromptTemplate } from "langchain/prompts";
 import {
-  RunnableSequence,
-  RunnablePassthrough,
-} from "langchain/schema/runnable";
+  ChatPromptTemplate,
+  MessagesPlaceholder,
+  AIMessagePromptTemplate,
+  HumanMessagePromptTemplate,
+} from "langchain/prompts";
+import { BufferMemory } from "langchain/memory";
+import { RunnableSequence } from "langchain/schema/runnable";
 import { StringOutputParser } from "langchain/schema/output_parser";
 import { formatDocumentsAsString } from "langchain/util/document";
+import { HuggingFaceTransformersEmbeddings } from "@langchain/community/embeddings/hf_transformers";
+import { RecursiveCharacterTextSplitter } from "langchain/text_splitter";
+
 import chalk from "chalk";
+import path from "path";
+import fs from "fs";
 
 import { env } from "@/lib/env";
-
-type ConversationalRetrievalQAChainInput = {
-  question: string;
-  chat_history: [string, string][];
-};
 
 export class AI {
   private model: ChatOpenAI;
   private vectorStore: any;
-  private questionTemplate: string;
-  private answerTemplate: string;
-  private chatHistory: [string, string][];
+  private memory: BufferMemory;
 
   constructor() {
     this.model = new ChatOpenAI({
       openAIApiKey: env.OPEN_AI_API_KEY,
-      // verbose: env.NODE_ENV != "production",
     });
     this.vectorStore = null;
-    this.questionTemplate = `Given the following conversation and a follow up question, rephrase the follow up question to be a standalone question, in English.
-      Chat History:
-      {chat_history}
-      Follow Up Input: {question}
-      Standalone question:
-    `;
-
-    this.answerTemplate = `Answer the question based only on the following context:
-      {context}
-
-      If there isn't enough information below, say you don't know. If asking a clarifying question to the user would help, ask the question.
-
-      Question: {question}
-     `;
-    this.chatHistory = [];
+    this.memory = new BufferMemory({
+      returnMessages: true,
+      memoryKey: "chat_history",
+    });
   }
 
   async load(filename: string): Promise<void> {
-    console.log(chalk.yellow("\nLoading VectorStore...\n"));
-    const loader = new TextLoader(filename);
-    const docs = await loader.load();
-    const vectorStore = await HNSWLib.fromDocuments(
-      docs,
-      new OpenAIEmbeddings({
-        openAIApiKey: env.OPEN_AI_API_KEY,
-        // verbose: env.NODE_ENV != "production",
-      })
-    );
-    this.vectorStore = vectorStore.asRetriever();
-  }
+    if (
+      fs.existsSync(
+        path.join(
+          __dirname,
+          "..",
+          "assets",
+          "store",
+          path.basename(filename).split(".")[0]
+        )
+      )
+    ) {
+      console.log(chalk.yellow("\nFound vector store from cache...\n"));
+      this.vectorStore = await HNSWLib.load(
+        path.join(__dirname, "..", "assets", "store", path.basename(filename)),
+        new HuggingFaceTransformersEmbeddings({
+          modelName: "Xenova/all-MiniLM-L6-v2",
+        })
+      );
+    } else {
+      console.log(chalk.yellow("\nLoading VectorStore...\n"));
+      const loader = new TextLoader(filename);
+      let docs = await loader.load();
+      const text_splitter = new RecursiveCharacterTextSplitter({
+        chunkSize: 2000,
+        chunkOverlap: 200,
+      });
 
-  private formatChatHistory(chatHistory: [string, string][]) {
-    const formattedDialogueTurns = chatHistory.map(
-      (dialogueTurn) =>
-        `Human: ${dialogueTurn[0]}\nAssistant: ${dialogueTurn[1]}`
-    );
-    return formattedDialogueTurns.join("\n");
+      docs = await text_splitter.splitDocuments(docs);
+
+      this.vectorStore = await HNSWLib.fromDocuments(
+        docs,
+        new HuggingFaceTransformersEmbeddings({
+          modelName: "Xenova/all-MiniLM-L6-v2",
+        })
+      );
+
+      console.log(chalk.yellow("\nSaving VectorStore...\n"));
+
+      await this.vectorStore.save(
+        path.join(__dirname, "..", "assets", "store", path.basename(filename))
+      );
+    }
+    this.vectorStore = this.vectorStore.asRetriever({
+      searchKwargs: { fetchK: 5 },
+    });
   }
 
   async ask(question: string): Promise<void> {
-    const CONDENSE_QUESTION_PROMPT = PromptTemplate.fromTemplate(
-      this.questionTemplate
-    );
+    const questionGeneratorTemplate = ChatPromptTemplate.fromMessages([
+      AIMessagePromptTemplate.fromTemplate(
+        "Given the following conversation about a transcript and a follow up question, rephrase the follow up question to be a standalone question."
+      ),
+      new MessagesPlaceholder("chat_history"),
+      AIMessagePromptTemplate.fromTemplate(`Follow Up Input: {question}
+Standalone question:`),
+    ]);
 
-    const ANSWER_PROMPT = PromptTemplate.fromTemplate(this.answerTemplate);
+    const combineDocumentsPrompt = ChatPromptTemplate.fromMessages([
+      AIMessagePromptTemplate.fromTemplate(
+        "Use the following pieces of context to answer the question at the end. If you don't know the answer, just say that you don't know, don't try to make up an answer.\n\n{context}\n\n"
+      ),
+      new MessagesPlaceholder("chat_history"),
+      HumanMessagePromptTemplate.fromTemplate("Question: {question}"),
+    ]);
 
-    const standaloneQuestionChain = RunnableSequence.from([
+    const combineDocumentsChain = RunnableSequence.from([
       {
-        question: (input: ConversationalRetrievalQAChainInput) =>
-          input.question,
-        chat_history: (input: ConversationalRetrievalQAChainInput) =>
-          this.formatChatHistory(input.chat_history),
+        question: (output: string) => output,
+        chat_history: async () => {
+          const { chat_history } = await this.memory.loadMemoryVariables({});
+          return chat_history;
+        },
+        context: async (output: string) => {
+          const relevantDocs = await this.vectorStore.getRelevantDocuments(
+            output
+          );
+          return formatDocumentsAsString(relevantDocs);
+        },
       },
-      CONDENSE_QUESTION_PROMPT,
+      combineDocumentsPrompt,
       this.model,
       new StringOutputParser(),
     ]);
 
-    const answerChain = RunnableSequence.from([
+    const conversationalQaChain = RunnableSequence.from([
       {
-        context: this.vectorStore.pipe(formatDocumentsAsString),
-        question: new RunnablePassthrough(),
+        question: (i: { question: string }) => i.question,
+        chat_history: async () => {
+          const { chat_history } = await this.memory.loadMemoryVariables({});
+          return chat_history;
+        },
       },
-      ANSWER_PROMPT,
+      questionGeneratorTemplate,
       this.model,
+      new StringOutputParser(),
+      combineDocumentsChain,
     ]);
-
-    const conversationalRetrievalQAChain =
-      standaloneQuestionChain.pipe(answerChain);
 
     console.log(chalk.yellow("\nAI is thinking..."));
 
-    const result = await conversationalRetrievalQAChain.invoke({
-      question: question,
-      chat_history: this.chatHistory,
+    const result = await conversationalQaChain.invoke({
+      question,
     });
 
-    this.chatHistory.push([question, result.content as string]);
+    await this.memory.saveContext(
+      {
+        input: question,
+      },
+      {
+        output: result,
+      }
+    );
 
-    console.log(chalk.greenBright(`\nSpeech GPT:`) + `\n${result.content}\n\n`);
+    console.log(chalk.greenBright(`\nSpeech GPT:`) + `\n${result}\n\n`);
   }
 }
